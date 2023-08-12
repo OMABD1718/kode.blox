ff34c6c3255996e5346753afdf3895922a90acd7
Sojith Sunny <sojithsunny29034@gmail.com>
2023-08-12 13:05:40 +0530

    first commit

---
+++ Home/Java_common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $togglecollapse = $('.toggle-collapse');
+
+    /* click event on toggle menu */
+    $togglecollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# kode.blox
+
+link: https://kodeblox.netlify.app/html/1introduction
